796def7222dc3fd3bbffd04e8c2185e959d4ac03
ram-221 <rameshmalli857.com>
2022-09-06 22:44:39 +0530

    Octal to Decimal Conversion

---
+++ OctalToDecimal.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class OctalToDecimal {
+
+	public static void main(String args[])
+	{      
+		Scanner sc = new Scanner(System.in);    
+		System.out.print("Enter a octal number : ");
+		int octal = sc.nextInt();
+		
+		int decimal = 0;		
+		int n = 0;  
+		while(octal > 0){
+			int temp = octal % 10;  
+			decimal += temp * Math.pow(8, n);  
+			octal = octal/10;  
+			n++;  
+		}
+		System.out.println("Decimal number : "+decimal); 
+		sc.close();   
+	}
+}
